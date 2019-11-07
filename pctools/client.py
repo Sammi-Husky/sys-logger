7e35c43dc5e92bd8b01d1175fe39e18c9d0b348d
Sammi Husky <sammi-husky@live.com>
2019-11-07 02:07:13 -0800

    Update to client for logging to file

--- pctools/client.py
+++ pctools/client.py
@@ -1,34 +1,55 @@
-import sys, socket, select
+import sys, socket, select, io
+import argparse
 
-serverAddressPort   = (sys.argv[1], 28280)
-bufferSize          = 256
+class MyParser(argparse.ArgumentParser): 
+   def error(self, message):
+      sys.stderr.write('error: %s\n' % message)
+      self.print_help()
+      sys.exit(2)
 
-sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-sock.settimeout(5)
+if __name__ == "__main__":
+    parser = MyParser(usage='%(prog)s [options] IP Address')
+    parser.add_argument("ipaddress", help="IP Address of the Nintendo Switch", metavar="IP Address")
+    parser.add_argument("-o", help="Sets output File", metavar="OUTPUT", nargs="?", dest="output", default=None)
+    args = parser.parse_args()
 
-while True:
-    connected = False
-    while not connected:
-        print('Sending handshake.')
-        # Send to server using created UDP socket
-        sock.sendto(str.encode("Hello Switch"), serverAddressPort)
-        try:
-            reply, addr = sock.recvfrom(bufferSize)
-            if reply.decode('utf-8') == 'Hello From sys-logger!':
-                print(f'Switch at {addr[0]} accepted our friend request')
-                connected = True
-        except socket.timeout:
-            continue
+    serverAddressPort   = (args.ipaddress, 28280)
+    bufferSize          = 256
 
-    print('Entering loop.')
-    while connected:
-        try:
-            ready_to_read, ready_to_write, in_error = select.select([sock,], [sock,], [], 5)
-        except select.error:
-            sock.shutdown(2)
-            sock.close()
-            print('Client has disconnected')
-            connected = False
-        if len(ready_to_read) > 0:
-            data = sock.recvfrom(bufferSize)
-            print(data[0].decode('utf-8'))
+    sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    sock.settimeout(5)
+
+    if args.output != None:
+        f = open(args.output,'w+')
+
+    while True:
+        connected = False
+        while not connected:
+            print('Sending handshake.')
+            # Send to server using created UDP socket
+            sock.sendto(str.encode("Hello Switch"), serverAddressPort)
+            try:
+                reply, addr = sock.recvfrom(bufferSize)
+                if reply.decode('utf-8'):
+                    connected = True
+                else:
+                    print(f'recived {reply.decode("utf-8")}')
+            except socket.timeout:
+                continue
+
+        print('Entering loop.')
+        while connected:
+            try:
+                ready_to_read, ready_to_write, in_error = select.select([sock,], [sock,], [], 5)
+            except select.error:
+                sock.shutdown(2)
+                sock.close()
+                print('Client has disconnected')
+                connected = False
+            if len(ready_to_read) > 0:
+                data = sock.recvfrom(bufferSize)
+                print(data[0].decode('utf-8').rstrip())
+                if args.output != None:
+                    f.write(data[0].decode('utf-8'))
+    if args.output != None:
+        f.close()
